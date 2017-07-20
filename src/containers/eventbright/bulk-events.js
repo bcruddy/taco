381e1ae0dee19da11aa9f01fce16f98a66d8b3b9
brian ruddy <briancruddy@gmail.com>
2017-07-19 22:20:46 -0400

    events table stuff

--- src/containers/eventbright/bulk-events.js
+++ src/containers/eventbright/bulk-events.js
@@ -2,21 +2,69 @@ import React from 'react';
 // import {push} from 'react-router-redux';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import store from '../../store';
 import {fetchEvents} from '../../modules/eventbright/events';
 import Leadspace from '../../ui/leadspace';
 
-const renderRows = (events) => {
-    return events && events.length ? events.map((event, index) => (
-        <p key={`event-${index}`}>{event.name.text}</p>
-    )): '';
+const renderContent = (props) => {
+    switch (props.status) {
+    case 'resolved':
+        return (
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>status</th>
+                        <th>Name</th>
+                        <th>Start</th>
+                        <th>End</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.events.map(event => (
+                        <tr key={`event-${event.id}`}>
+                            <td>{event.status}</td>
+                            <td>{event.name.text}</td>
+                            <td>{event.start.local}</td>
+                            <td>{event.end.local}</td>
+                            <td>{event.description.text}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        )
+        break;
+    case 'rejected':
+        return (
+            <div className="">
+                <p className="text-center">Uh oh! Something unexpected happened.</p>
+            </div>
+        );
+        break;
+    default:
+        return (
+            <div className="">
+                <p className="text-center">loading...</p>
+            </div>
+        );
+    }
 };
 
 const Editor = props => {
+    if (props.status === 'init') {
+        store.dispatch(fetchEvents());
+    }
+
     return (
         <section className="bulk-edit--event-bright">
             <Leadspace title="Event Bulk Editor" tagline="for editing eventbright events" />
-            <p>{props.status} - <a onClick={props.fetchEvents}>fetch events</a></p>
-            {renderRows(props.events)}
+            <main className="container-fluid">
+                <div className="row">
+                    <div className="col-md-12">
+                        {renderContent(props)}
+                    </div>
+                </div>
+            </main>
         </section>
     );
 };
--- src/modules/eventbright/events.js
+++ src/modules/eventbright/events.js
@@ -1,23 +1,32 @@
-export const FETCH_EVENTS = 'eventbright/events/FETCH_EVENTS';
-
-const ViewState = {
-    loading: 0,
-    success: 1,
-    failed: 2
-};
+export const FETCH_EVENTS_LOADING = 'eventbright/events/FETCH_EVENTS_LOADING';
+export const FETCH_EVENTS_RESOLVED = 'eventbright/events/FETCH_EVENTS_RESOLVED';
+export const FETCH_EVENTS_REJECTED= 'eventbright/events/FETCH_EVENTS_REJECTED';
 
 const initialState = {
-    status: ViewState.loading,
+    status: 'init',
     data: []
 };
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case FETCH_EVENTS:
+        case FETCH_EVENTS_LOADING:
             return {
                 ...state,
-                data: action.data,
-                status: action.status
+                status: 'loading',
+                data: []
+            };
+        case FETCH_EVENTS_RESOLVED:
+            console.log(action.data);
+            return {
+                ...state,
+                status: 'resolved',
+                data: action.data
+            };
+        case FETCH_EVENTS_REJECTED:
+            return {
+                ...state,
+                status: 'rejected',
+                data: []
             };
         default:
             return state;
@@ -26,15 +35,15 @@ export default (state = initialState, action) => {
 
 export const fetchEvents = () => {
         return dispatch => {
-            dispatch({type: FETCH_EVENTS, status: ViewState.loading});
+            dispatch({type: FETCH_EVENTS_LOADING});
 
             fetch('/api/eventbright/events')
                 .then(res => res.json())
                 .then(data => {
-                    dispatch({type: FETCH_EVENTS, data: data.events, status: ViewState.success});
+                    dispatch({type: FETCH_EVENTS_RESOLVED, data: data.events});
                 })
-                .catch(err => {
-                    dispatch({type: FETCH_EVENTS, status: ViewState.failed});
+                .catch(error => {
+                    dispatch({type: FETCH_EVENTS_REJECTED, error});
                 });
         };
 };
