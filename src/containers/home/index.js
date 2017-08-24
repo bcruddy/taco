9d21871c41c301760344f4a736184dca382e638d
brian ruddy <briancruddy@gmail.com>
2017-08-24 00:08:10 -0400

    add leadspace tagline

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Leadspace from '../../ui/leadspace';
 
+const tagline = `probably not the worst crypto resource you've found so far. probably.`;
 const Home = props => (
     <section className="taco--counter">
-        <Leadspace title="Home" />
+        <Leadspace title="Home" tagline={tagline} />
         <main className="container">
             <div className="row">
                 <div className="col-md-12">
-                    <p>hello world.</p>
+                    <p>good luck.</p>
                 </div>
             </div>
         </main>
